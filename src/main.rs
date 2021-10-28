434aeefda21f13220a71c171cd89134bd10dfa90
Nick Driscoll <nick.a.mavros@gmail.com>
2021-10-28 16:31:17 -0400

    simple

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::net::{UdpSocket, TcpListener};
+use std::net::{UdpSocket};
 use std::time::Duration;
-
-
 
 fn main() {
     println!("Starting server");
-
-    let listener = TcpListener::bind("0.0.0.0:6969").unwrap();
-    for stream in listener.incoming() {
-
-    }        
+    let listener = UdpSocket::bind("0.0.0.0:6969").unwrap();
+    
+    let mut packet_buffer = [0u8; 16];
+    while let Ok((size, socket_addr)) = listener.recv_from(&mut packet_buffer) {
+        println!("Received {} bytes\n{:?}", size, packet_buffer);        
+    }
 }
